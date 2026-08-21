251539217ac4641160c773fee4b5ce34aacdfe11
Shadrac <shadrac@fluid.app>
2026-08-21 13:24:04 -0600

    Update Conform formatter fallback syntax

--- shared/stow/nvim/.config/nvim/lua/plugins/conform.lua
+++ shared/stow/nvim/.config/nvim/lua/plugins/conform.lua
@@ -36,9 +36,8 @@ return { -- Autoformat
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
-			-- You can use a sub-list to tell conform to run *until* a formatter
-			-- is found.
-			javascript = { { "prettierd", "prettier" } },
+			-- Use the first available formatter in the list.
+			javascript = { "prettierd", "prettier", stop_after_first = true },
 			javascriptreact = { "prettier" },
 			typescript = { "prettier" },
 			typescriptreact = { "prettier" },
